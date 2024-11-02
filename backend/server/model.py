c7cccc75e51f07770e0e112a3c8b83b8c6a55f35
BogdanJ33 <bogdanjanosevic@gmail.com>
2024-11-02 13:07:10 +0100

    Added some code, and added get language and get aggregation level

--- backend/server/model.py
+++ backend/server/model.py
@@ -1,5 +1,5 @@
 from groq import Groq
-from metadata import Metadata, GeneralMetadata
+from metadata import *
 import os
 import PyPDF2
 import docx
@@ -46,6 +46,13 @@ class TextAnalyzer:
         metadata_instance = Metadata()
 
         metadata_instance.general = self.get_general_data(file, text, model, temperature, max_tokens, top_p)
+        metadata_instance.lifeCycle = self.get_life_cycle_data(file, text, model, temperature, max_tokens, top_p)
+        metadata_instance.tehnical = self.get_tehnical_data(file, text, model, temperature, max_tokens, top_p)
+        metadata_instance.educational = self.get_educational_data(file, text, model, temperature, max_tokens, top_p)
+        metadata_instance.rights = self.get_rights_data(file, text, model, temperature, max_tokens, top_p)
+        metadata_instance.relation = self.get_relation_data(file, text, model, temperature, max_tokens, top_p)
+        metadata_instance.classification = self.get_classification_data(file, text, model, temperature, max_tokens, top_p)
+
         # do for others
         return metadata_instance
 
@@ -54,8 +61,44 @@ class TextAnalyzer:
         general.keywords = self.get_keywords(
             text, model, temperature, max_tokens, top_p
         )
+        general.language = self.get_language(
+            text, model, temperature, max_tokens, top_p
+        )
+        general.aggregation_level = self.get_aggregation_level(
+            text, model, temperature, max_tokens, top_p
+        )
         
         return general
+    
+    def get_life_cycle_data(self, file, text, model, temperature, max_tokens, top_p):
+        life_cycle = LifeCycleMetadata()
+        # TO DO
+        return life_cycle
+    
+    def get_tehnical_data(self, file, text, model, temperature, max_tokens, top_p):
+        tehnical = TehnicalMetadata()
+        # TO DO
+        return tehnical
+    
+    def get_educational_data(self, file, text, model, temperature, max_tokens, top_p):
+        educational = EducationalMetadata()
+        # TO DO
+        return educational
+    
+    def get_rights_data(self, file, text, model, temperature, max_tokens, top_p):
+        rights = RightsMetadata()
+        # TO DO
+        return rights
+    
+    def get_relation_data(self, file, text, model, temperature, max_tokens, top_p):
+        relation = RelationMetadata()
+        # TO DO
+        return relation
+    
+    def get_classification_data(self, file, text, model, temperature, max_tokens, top_p):
+        classification = ClassificationMetadata()
+        # TO DO
+        return classification
 
     def get_keywords(self, text, model, temperature, max_tokens, top_p):
         """Generate keywords from the given text using the Groq API."""
@@ -66,6 +109,46 @@ class TextAnalyzer:
                 {
                     "role": "system",
                     "content": "Extract keywords with bullet points. Dont add any aditional text",
+                },
+            ],
+            temperature=temperature,
+            max_tokens=max_tokens,
+            top_p=top_p,
+            stream=False,
+        )
+        return completion.choices[0].message.content.strip()
+    
+    def get_language(self, text, model, temperature, max_tokens, top_p):
+        """Get language from the given text using the Groq API."""
+        completion = self.client.chat.completions.create(
+            model=model,
+            messages=[
+                {"role": "user", "content": text},
+                {
+                    "role": "system",
+                    "content": "Extract language of the text. Dont add any aditional text just language",
+                },
+            ],
+            temperature=temperature,
+            max_tokens=max_tokens,
+            top_p=top_p,
+            stream=False,
+        )
+        return completion.choices[0].message.content.strip()
+    
+    def get_aggregation_level(self, text, model, temperature, max_tokens, top_p):
+        """Get aggregation level from the given text using the Groq API."""
+        completion = self.client.chat.completions.create(
+            model=model,
+            messages=[
+                {"role": "user", "content": text},
+                {
+                    "role": "system",
+                    "content": """Extract aggregation level of the text with just level, no additional text. Aggregation level is The functional granularity of this learning object. These are possible values: 
+1: the smallest level of aggregation, e.g., raw media data or fragments.
+2: a collection of level 1 learning objects, e.g., a lesson.
+3: a collection of level 2 learning objects, e.g., a course.
+4: the largest level of granularity, e.g., a set of courses that lead to a certificate."""
                 },
             ],
             temperature=temperature,
